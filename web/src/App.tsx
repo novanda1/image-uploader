f77885d75b01fba461b25dc9b3e5b57648d42bb4
Novanda Ahsan <novandaahsan1@gmail.com>
2022-09-20 15:32:02 +0700

    web: add font family

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function App() {
   return (
-    <div className="min-h-screen min-w-[100vw] bg-white flex items-center justify-center">
+    <div className="font-sans min-h-screen min-w-[100vw] bg-white flex items-center justify-center">
       <div className="max-w-[402px] bg-white shadow-[0_4px_12px_0_rgba(0,0,0,0.1)] py-9 px-8 rounded-[12px]">
         oh hi
       </div>
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          "Poppins",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+          "Noto Sans",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+      },
+    },
   },
   plugins: [],
 };
