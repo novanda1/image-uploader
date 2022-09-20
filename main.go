0fe19c1dcdb57c46e1a76431e50375e00636bde2
Novanda Ahsan <novandaahsan1@gmail.com>
2022-09-20 14:44:25 +0700

    log: log fatal when env not found

--- main.go
+++ main.go
@@ -13,7 +13,12 @@ import (
 )
 
 func main() {
-	config, _ := conf.LoadGlobal(".env")
+	config, err := conf.LoadGlobal(".env")
+
+	if err != nil {
+		logrus.Fatalf("failed load .env: %v", err)
+	}
+
 	api := api.NewApi(config)
 
 	l := fmt.Sprintf("%v:%v", config.API.Host, config.API.Port)
