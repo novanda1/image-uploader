fd871882c986ab8801ebe0406a92f50ce22fdcfe
Novanda Ahsan <novandaahsan1@gmail.com>
2022-09-20 15:42:47 +0700

    web: add font (montserrat)

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,3 +1,20 @@
+const fontFallback = [
+  "ui-sans-serif",
+  "system-ui",
+  "-apple-system",
+  "BlinkMacSystemFont",
+  "Segoe UI",
+  "Roboto",
+  "Helvetica Neue",
+  "Arial",
+  "Noto Sans",
+  "sans-serif",
+  "Apple Color Emoji",
+  "Segoe UI Emoji",
+  "Segoe UI Symbol",
+  "Noto Color Emoji",
+];
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
@@ -13,23 +30,8 @@ module.exports = {
         light: "#fafafb",
       },
       fontFamily: {
-        sans: [
-          "Poppins",
-          "ui-sans-serif",
-          "system-ui",
-          "-apple-system",
-          "BlinkMacSystemFont",
-          "Segoe UI",
-          "Roboto",
-          "Helvetica Neue",
-          "Arial",
-          "Noto Sans",
-          "sans-serif",
-          "Apple Color Emoji",
-          "Segoe UI Emoji",
-          "Segoe UI Symbol",
-          "Noto Color Emoji",
-        ],
+        sans: ["Poppins", ...fontFallback],
+        montserrat: ["Montserrat", ...fontFallback],
       },
     },
   },
